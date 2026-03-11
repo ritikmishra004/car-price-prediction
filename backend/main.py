af23748bf305deef9bfd4657bbb147defd6ff2d4
ritikmishra004 <Ritikmishra1004@gmail.com>
2026-03-11 12:17:13 +0530

    Car price prediction web app

---
+++ backend/main.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import pickle
+import pandas as pd
+import numpy as np
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+model = pickle.load(open("../artifacts/car_price_model.pkl","rb"))
+
+# serve frontend
+app.mount("/static", StaticFiles(directory="../frontend"), name="static")
+
+@app.get("/")
+def serve_frontend():
+    return FileResponse("../frontend/index.html")
+
+@app.post("/predict")
+def predict(data: dict):
+
+    km_per_year = data["km_driven"] / data["car_age"]
+
+    input_df = pd.DataFrame([{
+        "brand": data["brand"],
+        "fuel": data["fuel"],
+        "seller_type": data["seller_type"],
+        "transmission": data["transmission"],
+        "owner": data["owner"],
+        "km_driven": data["km_driven"],
+        "car_age": data["car_age"],
+        "km_per_year": km_per_year
+    }])
+
+    pred = model.predict(input_df)
+
+    price = int(np.exp(pred)[0])
+
+    return {"predicted_price": price}
---
+++ frontend/script.js
@@ -0,0 +1,30 @@
+async function predict(){
+
+const data = {
+
+brand: document.getElementById("brand").value,
+fuel: document.getElementById("fuel").value,
+seller_type: document.getElementById("seller").value,
+transmission: document.getElementById("transmission").value,
+owner: document.getElementById("owner").value,
+km_driven: parseInt(document.getElementById("km").value),
+car_age: parseInt(document.getElementById("age").value)
+
+}
+
+const response = await fetch("http://127.0.0.1:8000/predict",{
+
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body: JSON.stringify(data)
+
+})
+
+const result = await response.json()
+
+document.getElementById("result").innerText =
+"Estimated Price: ₹ " + result.predicted_price
+
+}
